6771a9e0ae9ad17de9c3a3210ac8666ce0784e60
Alex Schneidman <alexmarkschneidman@gmail.com>
2024-04-27 10:54:20 -0700

    starting the repl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io::{self, Write};
+
+fn print_prompt() {
+    print!("alexdb > ");
+    io::stdout().flush().expect("Failed to flush stdout");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    let mut line = String::new();
+    loop {
+        print_prompt();
+        stdin.read_line(&mut line).expect("Failed to read line");
+        match line.as_str() {
+            "" | ".exit\n" => {
+                return;
+            },
+            l => println!("Unrecognized command '{}'", l.trim()),
+        }
+        line.clear();
+    }
 }
