db1d982b7241292adb984bd5cecf3a89f92c2300
Alex Schneidman <alexmarkschneidman@gmail.com>
2024-04-27 11:27:03 -0700

    prepare statements

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,38 @@
-use std::io::{self, Write};
+use std::{fmt::format, io::{self, Write}};
+
+enum StatementType {
+    INSERT,
+    SELECT
+}
+
+struct Statement {
+    typ: StatementType,
+}
 
 fn print_prompt() {
     print!("alexdb > ");
     io::stdout().flush().expect("Failed to flush stdout");
+}
+
+fn prepare_statement(line: &str) -> Result<Statement, String> {
+    if line.starts_with("INSERT ") {
+        return Ok(Statement {
+                typ: StatementType::INSERT,
+            });
+    } else if (line.starts_with("SELECT ")) {
+        return Ok(Statement {
+            typ: StatementType::SELECT,
+        });
+    }
+    return Err(format!("Unrecognized statement {line}"));
+}
+
+fn execute_statement(statement: Statement) -> Result<(), String> {
+    match statement.typ {
+        StatementType::INSERT => println!("Would execute INSERT"),
+        StatementType::SELECT => println!("Would execute SELECT"),
+    }
+    return Ok(());
 }
 
 fn main() {
@@ -15,7 +45,13 @@ fn main() {
             "" | ".exit\n" => {
                 return;
             },
-            l => println!("Unrecognized command '{}'", l.trim()),
+            l => match prepare_statement(l) {
+                Ok(statement) => match execute_statement(statement) {
+                    Ok(()) => println!("Executed."),
+                    Err(msg) => println!("Failed to execute statement: {}", msg),
+                },
+                Err(msg) => println!("Failed to prepare statement: {}", msg),
+            },
         }
         line.clear();
     }
